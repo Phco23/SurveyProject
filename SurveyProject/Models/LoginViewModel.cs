b0172276a7c67780b1ca178a340183bc90fe35dd
lequangnam2701 <142825327+lequangnam2701@users.noreply.github.com>
2024-12-06 08:57:52 +0700

    login

---
+++ SurveyProject/Models/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SurveyProject.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "UserName is required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+        public bool RememberLogin { get; set; }
+
+    }
+
+}
