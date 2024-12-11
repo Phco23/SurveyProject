c2f38fc4e628c43a822df545a842cd594c927645
VanNgo1610 <142959206+VanNgo1610@users.noreply.github.com>
2024-12-11 11:55:03 +0700

    Add files via upload

---
+++ SurveyProject/Repository/ContestRepository.cs
@@ -0,0 +1,29 @@
+﻿using SurveyProject.Migrations;
+using SurveyProject.Models;
+using System;
+
+namespace SurveyProject.Repository
+{
+    public class ContestRepository : IContestRepository
+    {
+        private readonly SurveyDbContext _context;
+
+        public ContestRepository(SurveyDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ContestModel> AddContestAsync(ContestModel contest)
+        {
+            _context.contests.Add(contest);
+            await _context.SaveChangesAsync();
+            return contest;
+        }
+
+        public async Task<ContestModel?> GetContestByIdAsync(int id)
+        {
+            return await _context.contests.FindAsync(id);
+        }
+    }
+
+}
