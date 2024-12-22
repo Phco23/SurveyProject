9126220adb7d2590a5cdcffc8f473e01fef30f08
VanNgo1610 <142959206+VanNgo1610@users.noreply.github.com>
2024-12-22 10:06:26 +0700

    Update SurveyDbContext.cs

--- SurveyProject/Migrations/SurveyDbContext.cs
+++ SurveyProject/Migrations/SurveyDbContext.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using SurveyProject.Models;
+using System.Reflection;
+using System;
 
 namespace SurveyProject.Migrations
 {
@@ -7,6 +9,35 @@ namespace SurveyProject.Migrations
     {
         public SurveyDbContext(DbContextOptions<SurveyDbContext> options) : base(options) { }
 
-        public DbSet<ContestModel> contests { get; set; }
+        public DbSet<ContestModel> Contests { get; set; }
+        public DbSet<WinnerModel> Winners { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<WinnerModel>(entity =>
+            {
+                entity.HasKey(w => w.Id);
+                entity.Property(w => w.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+                entity.Property(w => w.RollNumber)
+                    .IsRequired()
+                    .HasMaxLength(20);
+                entity.HasOne(w => w.Contest)
+                    .WithMany(c => c.Winners)
+                    .HasForeignKey(w => w.ContestId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            modelBuilder.Entity<ContestModel>(entity =>
+            {
+                entity.HasKey(c => c.Id);
+                entity.Property(c => c.Title)
+                    .IsRequired()
+                    .HasMaxLength(200);
+                entity.Property(c => c.Description)
+                    .HasMaxLength(1000);
+            });
+        }
     }
 }
