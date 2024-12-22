8fb57ad98f6f4d866ce1ba962acb6736c723f629
VanNgo1610 <142959206+VanNgo1610@users.noreply.github.com>
2024-12-22 10:05:49 +0700

    Add files via upload

---
+++ SurveyProject/Models/WinnerModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace SurveyProject.Models
+{
+    public class WinnerModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string RollNumber { get; set; } = string.Empty;
+        public int ContestId { get; set; }
+
+        // Navigation property
+        public ContestModel Contest { get; set; } = null!;
+    }
+
+}
