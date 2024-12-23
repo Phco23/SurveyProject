86ea9bd7d9bf6dcba5020cc911fdf57f74c5d035
VanNgo1610 <142959206+VanNgo1610@users.noreply.github.com>
2024-12-23 09:46:50 +0700

    Update IContestRepository.cs

--- SurveyProject/Repository/IContestRepository.cs
+++ SurveyProject/Repository/IContestRepository.cs
@@ -1,5 +1,6 @@
-﻿using SurveyProject.Models;
-using System;
+using SurveyProject.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace SurveyProject.Repository
 {
@@ -7,5 +8,8 @@ namespace SurveyProject.Repository
     {
         Task<ContestModel> AddContestAsync(ContestModel contest);
         Task<ContestModel?> GetContestByIdAsync(int id);
+        Task<WinnerModel> AddWinnerAsync(WinnerModel winner);
+        Task<WinnerModel?> GetWinnerByIdAsync(int id);
+        Task<IEnumerable<WinnerModel>> GetWinnersByContestIdAsync(int contestId);
     }
 }
