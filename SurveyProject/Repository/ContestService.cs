5ac22aa215e0193bfb5cf920de89d9a916bf186b
VanNgo1610 <142959206+VanNgo1610@users.noreply.github.com>
2024-12-23 09:45:11 +0700

    Update ContestService.cs

--- SurveyProject/Repository/ContestService.cs
+++ SurveyProject/Repository/ContestService.cs
@@ -4,28 +4,37 @@ using System;
 namespace SurveyProject.Repository
 {
     public class ContestService : IContestService
+{
+    private readonly IContestRepository _contestRepository;
+
+    public ContestService(IContestRepository contestRepository)
     {
-        private readonly IContestRepository _contestRepository;
-
-        public ContestService(IContestRepository contestRepository)
-        {
-            _contestRepository = contestRepository;
-        }
-
-        public async Task<ContestModel> AddContestAsync(ContestModel contest)
-        {
-            if (string.IsNullOrWhiteSpace(contest.Title))
-            {
-                throw new ArgumentException("Contest title cannot be empty.");
-            }
-
-            return await _contestRepository.AddContestAsync(contest);
-        }
-
-        public async Task<ContestModel?> GetContestByIdAsync(int id)
-        {
-            return await _contestRepository.GetContestByIdAsync(id);
-        }
+        _contestRepository = contestRepository;
     }
 
+    public async Task<ContestModel> AddContestAsync(ContestModel contest)
+    {
+        return await _contestRepository.AddContestAsync(contest);
+    }
+
+    public async Task<ContestModel?> GetContestByIdAsync(int id)
+    {
+        return await _contestRepository.GetContestByIdAsync(id);
+    }
+
+    public async Task<WinnerModel> AddWinnerAsync(WinnerModel winner)
+    {
+        return await _contestRepository.AddWinnerAsync(winner);
+    }
+
+    public async Task<WinnerModel?> GetWinnerByIdAsync(int id)
+    {
+        return await _contestRepository.GetWinnerByIdAsync(id);
+    }
+
+    public async Task<IEnumerable<WinnerModel>> GetWinnersByContestIdAsync(int contestId)
+    {
+        return await _contestRepository.GetWinnersByContestIdAsync(contestId);
+    }
+}
 }
