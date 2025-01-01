708d7b67e72918d8efd7948f1209b0464507f8be
nhacne <142861451+nhacne@users.noreply.github.com>
2025-01-02 01:10:54 +0700

    update views

---
+++ SurveyProject/Migrations/20250101164050_IntialCreaten_1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SurveyProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class IntialCreaten_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Response",
+                table: "Feedbacks",
+                newName: "Email");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Feedbacks",
+                newName: "Response");
+        }
+    }
+}
